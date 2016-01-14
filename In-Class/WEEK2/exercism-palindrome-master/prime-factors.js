9963dc285805a055c94a1d7b653fd81560bd3557
Jon Hammond <jonh1016@gmail.com>
2016-01-14 09:27:57 -0700

    Added my modified solution to the master directory.

--- In-Class/WEEK2/exercism-palindrome-master/prime-factors.js
+++ In-Class/WEEK2/exercism-palindrome-master/prime-factors.js
@@ -1,15 +1,46 @@
-function primeFactors (num) {
-    var input = num;
-    var factors = [];
-    for (var i = 2; i <= input; i++) {
-        while(input % i === 0 && input !== 1) {
-            factors.push(i);
-            input = input/i;
-        }
-    }
-    return factors;
-}
+// our code
+function myFn (number) {
+  var input = numbers;
+  var factors = [];
+  for (var i=0; i <= input; i++) {
+    while (input === 0 && input !== 1) {
+        factors.push(i);
+        input = input/i;
+      };
+    };
+  };
+  return factors;
+};
 
 module.exports = {
-    for : primeFactors
-}
+  for: myFn
+};
+  
+
+
+
+//   while (i < number) {
+//     if (number % i === 0) {
+//       factors.push(i);
+//     }
+//     // else if (number === 0) {
+//     //   factors.push(i);
+//     // }
+//     else {
+//       factors.push(number);
+//     };
+//     i++;
+//   };
+//   return factors;
+// };
+
+  // for (var i = 2; i <= arg; i++) {
+  //   if (arg % i === 0) {
+  //     factors.push(i);
+  //   }
+    // else {
+    //   return [];
+    // }
+//   };
+//   return factors;
+// };
