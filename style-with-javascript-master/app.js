3596ab2e9da0ea810789cb5d28bdad41914fcb4f
Jon Hammond <jonh1016@gmail.com>
2016-01-14 16:37:50 -0700

    So close to having the page finished. Damn.

--- style-with-javascript-master/app.js
+++ style-with-javascript-master/app.js
@@ -1,1 +1,44 @@
-// Write your code in this file
+var body = document.getElementsByTagName('body')[0];
+body.style.width = ('600px');
+
+var header = document.getElementsByTagName('nav')[0];
+header.style.backgroundColor = ('black');
+header.style.color = ('grey');
+header.style.textAlign = ('center');
+header.style.marginBottom = ('0px')
+header.style.paddingBottom = ('.02px')
+// header.style.paddingBottom = ('1px');
+
+var firstSquare = document.getElementsByTagName('div')[0];
+firstSquare.style.height = ('200px');
+firstSquare.style.width = ('200px');
+firstSquare.style.backgroundColor = ('green');
+firstSquare.style.display = ('inline-block');
+firstSquare.style.margin = ('-20px -5px -20px 0px');
+firstSquare.style.boxSizing = ('border-box')
+
+var secondSquare = document.getElementsByTagName('div')[1];
+secondSquare.style.height = ('200px');
+secondSquare.style.width = ('200px');
+secondSquare.style.backgroundColor = ('blue');
+secondSquare.style.display = ('inline-block');
+secondSquare.style.margin = ('-20px -5px -20px 0px');
+secondSquare.style.boxSizing = ('border-box')
+
+var thirdSquare = document.getElementsByTagName('div')[2];
+thirdSquare.style.height = ('200px');
+thirdSquare.style.width = ('200px');
+thirdSquare.style.backgroundColor = ('purple');
+thirdSquare.style.display = ('inline-block');
+thirdSquare.style.margin = ('-20px -5px -20px 0px');
+thirdSquare.style.boxSizing = ('border-box')
+thirdSquare.style.paddingRight = ('3px');
+
+var footer = document.getElementsByTagName('footer')[0];
+footer.style.backgroundColor = ('grey');
+footer.style.color = ('black');
+footer.style.textAlign = ('center');
+footer.style.paddingBottom = ('1px')
+footer.style.paddingTop = ('1px')
+// footer.style.float = ('clear: both');
+
