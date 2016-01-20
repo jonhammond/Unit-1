d211f8c0fd7c93cb0f4d6918f2c273424d91b5a8
Jon Hammond <jonh1016@gmail.com>
2016-01-20 14:14:23 -0700

    Added Domingo Ayala to the OOPing exercsise.

--- Week-3/chapstick-OOP.js
+++ Week-3/chapstick-OOP.js
@@ -1,31 +1,66 @@
-var Chapstick = function () {
-  this.color = 'Yellow';
-  this.volume = 100;
-  this.capped = true;
-  this.cap = function () {
-    this.capped = true;
-  };
-  this.uncap = function () {
-    this.capped = false;
-  };
-}
-
-var myChapstick = new Chapstick();
-
-// myChapstick.uncap = function () {
-//   this.capped = false;
-// }
-
-// myChapstick.cap = function () {
+// var Chapstick = function () {
+//   this.color = 'Yellow';
+//   this.volume = 100;
 //   this.capped = true;
+//   this.cap = function () {
+//     this.capped = true;
+//   };
+//   this.uncap = function () {
+//     this.capped = false;
+//   };
+//   this.use = function() {
+//     this.volume -= 1;
+//   }
 // }
 
-console.log(myChapstick);
+// var myChapstick = new Chapstick();
 
-myChapstick.uncap();
+// // myChapstick.uncap = function () {
+// //   this.capped = false;
+// // }
 
-console.log('myChapstick uncapped = ', myChapstick)
+// // myChapstick.cap = function () {
+// //   this.capped = true;
+// // }
 
-var chapstick2 = new Chapstick();
+// console.log(myChapstick);
 
-console.log(chapstick2);
+// myChapstick.use();
+
+// console.log('myChapstick uncapped = ', myChapstick)
+
+// var chapstick2 = new Chapstick();
+
+// console.log(chapstick2);
+//
+
+var Beisbol = function(playerInfo) {
+  this.firstName = playerInfo.firstName;
+  this.preferredName = playerInfo.preferredName || playerInfo.firstName;
+  this.lastName = playerInfo.lastName;
+  this.fullName = playerInfo.firstName + ' ' + playerInfo.lastName;
+  this.height = playerInfo.heightInInches;
+  this.energy = 100;
+  this.happiness = 100;
+
+  this.say = function(phrase) {
+    return phrase;
+  };
+
+  this.sayHeight = function() {
+    var feet = Math.floor(this.height/12)
+    var inches = this.height%12;
+    return 'My name is ' + this.fullName + '. ' + 'People call me ' + this.preferredName + '. '+ 'I am ' + feet + ' ft ' + inches + ' inches tall.';
+  };
+};
+
+var player = {
+  firstName: 'Domingo',
+  preferredName: 'The Homerun King',
+  lastName: 'Ayala',
+  heightInInches: 78
+};
+
+var Domingo = new Beisbol(player);
+
+console.log(Domingo.sayHeight());
