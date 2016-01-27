98f6d15f95448b92825e2ff69269ca9720c9d3b5
Jon Hammond <jonh1016@gmail.com>
2016-01-27 13:38:26 -0700

    Added more to the stripe-api validation to check for valid credit card

--- Week-4/stripe-api-intro-master/src/js/main.js
+++ Week-4/stripe-api-intro-master/src/js/main.js
@@ -2,4 +2,26 @@
 
 $(document).on('ready', function() {
   console.log('sanity check!');
+  Stripe.setPublishableKey('pk_test_ag8tmFNIwti0SOME2bwILOpq');
 });
+
+$('.order').on('click', function () {
+  var cardInfo = {
+  number: $('.card-number').val(),
+  cvc: $('.card-cvc').val(),
+  exp_month: $('.card-expiry').val().split('/')[0],
+  exp_year: $('.card-expiry').val().split('/')[1]
+  };
+
+  Stripe.card.createToken(cardInfo, stripeResponseHandler);
+
+});
+
+function stripeResponseHandler(status, response) {
+  if (response.error) {
+    console.log(response.error.message);
+  }
+  else {
+    console.log(response);
+  }
+}
