db26dcdf31c06ae92dbb27b209db987837fb05a4
Jon Hammond <jonh1016@gmail.com>
2016-01-27 16:51:03 -0700

    Email validator finally works. Still needs a few touch-ups

--- Week-4/html-forms-validation-master/src/js/main.js
+++ Week-4/html-forms-validation-master/src/js/main.js
@@ -2,62 +2,74 @@
 
 $(document).on('ready', function() {
 
-
-  var userEmail = document.getElementById('email');
-
-  function validateEmail (email) {
-    if (email.indexOf === -1) {
-      return false;
-    }
-    var emailArray = email.split('@');
-    emailArray.filter(function(val) {
-      if(emailArray[1].indexOf('.') === -1) {
-        // console.log(val);
-        // console.log(emailArray);
-        console.log("false");
-        return false;
+  // forEach function for warning border
+  ['preferredName', 'lastName', 'email'].forEach(function(inputName) {
+    var $input = $('input[name="' + inputName + '"]');
+    $input.on('blur', function() {
+      var val = $input.val();
+      if (!val) {
+        $input.addClass('warning');
       }
       else {
-        console.log("true");
-        return true;
+        $input.removeClass('warning');
+        $input.addClass('allGood');
       }
+
     });
-  }
+  });
+  // End of forEach for warning border
 
-  validateEmail(userEmail);
+  // Declare function validateEmail
+  function validateEmail (email) {
+    var emailArray = email;
+    var emailArraySplit = email.split('@');
+    console.log(emailArray);
 
-  // wes
-  // $email = $('input[type="email"]');
-  // $email.on('blur', function() {
-  //   var val = $email.val();
-  //   toggleClass;
-  //   })
-
-  //"hello" --> false
-  //"hello@" --> false
-  //"hello@ok" --> false
-  //"hello@ok." --> false
-  //hello@ok.com" --> true
-
-//   console.log('sanity check!');
-//   $('form').on('submit', function(event) {
-//     event.preventDefault();
-//     // console.log("AAAAHHHH!!!!");
-//   }
-// );
-
-
-  ['preferredName', 'lastName'].forEach(function(inputName) {
-    var $input = $('input[name="' + inputName + '"]');
-  $input.on('blur', function() {
-    // console.log("WOOOOOOOOO!!!!");
-    var val = $input.val();
-    if (!val) {
-      $input.addClass('warning');
+    if (emailArray === "") {
+      console.log("No email provided, returns false");
+      return false;
+    }
+    else if (emailArraySplit[1].indexOf('.') === -1) {
+      console.log("Missing a '.' after the '@', returns false");
+      return false;
     }
     else {
-      $input.removeClass('warning');
+      console.log("Email is valid, returns true");
+      return true;
     }
+  }
+  // End of function validateEmail
+
+
+  // Helper function alertUser
+  function alertUser(isValid) {
+    if (isValid) {
+      console.log('true');
+    }
+    else {
+      alert("Your email format is incorrect. Please ensure that you are including the '@' and the '.'. For example, myemail@domain.com");
+      console.log("false");
+    }
+  }
+  // End of helper function
+
+  //Declare preventDefault for form on submit
+  $('form').on('submit', function(event) {
+    event.preventDefault();
+      // console.log("preventDefault event is running");
+    }
+  );
+  //End of preventDefault
+
+
+
+  // Event listener for clicking submit button, call validateEmail
+  $('.submit').on('click', function() {
+    var userEmail = $('.email').val();
+    var isValid = validateEmail(userEmail);
+    alertUser(isValid);
   });
+  // End event listener for clicking submit button
+
 });
-});
+  // End document.ready
