4fe3b25bbf646012e591b45764fa0e6881b85f6c
Jon Hammond <jonh1016@gmail.com>
2016-01-29 22:29:27 -0700

    Added a lot of stuff and forgot commits...

--- Personal-Project/mySite/src/js/main.js
+++ Personal-Project/mySite/src/js/main.js
@@ -1,5 +1,8 @@
 // add scripts
 
 $(document).on('ready', function() {
+  $('.submitClick').on('click', function(){
+  $('#middle').append('<img id="images", src=""../GIF-final/alligator.gif"" />');
   console.log('sanity check!');
+  });
 });
