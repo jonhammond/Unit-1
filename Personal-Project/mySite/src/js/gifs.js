b5752fc1fd4e23953a99446bec43293a631eceaa
Jon Hammond <jonh1016@gmail.com>
2016-02-02 16:50:56 -0700

    Added neon style to site, changed the moonwalk stick figure. Currently adding button to add another carousel for a dance off.

---
+++ Personal-Project/mySite/src/js/gifs.js
@@ -0,0 +1,64 @@
+$(document).on('ready', function () {
+  console.log(gifStr(MyGifs.a.name, MyGifs.a.caption));
+});
+
+function gifStr (imageName, myCaption) {
+  return '<div class="item" align="center"><img src="./gifs/' + imageName + '.gif" alt="' + imageName + '"><div class="carousel-caption" align="center">' + myCaption + '</div></div>';
+}
+
+var MyGifs = {
+  a: {
+    name: 'alligator',
+    caption: 'A - The Alligator'},
+  b: {name: 'butterfly',
+      caption: 'B - The Butterfly'},
+  c: {name:'crankthat',
+      caption: 'C - Crank That (Soulja Boy)'},
+  d: {name: 'dab',
+      caption: 'D - The Dab'},
+  e: {name: 'elbowsup',
+      caption: 'E - Elbows Up'},
+  f: {name: 'funkychicken',
+      caption: 'F - The Funky Chicken'},
+  g: {name: 'gangnamstyle',
+      caption: 'G - Gangnam Style'},
+  h: {name: 'harlemshake',
+      caption: 'H - The Harlem Shake'},
+  i: {name: 'error',
+      caption: 'No Caption'},
+  j: {name: 'jump',
+      caption: 'J - Jump!'},
+  k: {name: 'error',
+      caption: 'No Caption'},
+  l: {name: 'leanback',
+      caption: 'L - Lean Back'},
+  m: {name: 'moonwalk-color',
+      caption: 'M - The Moonwalk'},
+  n: {name: 'naenae',
+      caption: 'N - The Nae-Nae'},
+  o: {name: 'error',
+      caption: 'No Caption'},
+  p: {name: 'error',
+      caption: 'No Caption'},
+  q: {name: 'queenbee',
+      caption: 'Q - Queen Bee (Beyonce)'},
+  r: {name: 'error',
+      caption: 'No Caption'},
+  s: {name: 'stankyleg',
+      caption: 'S - The Stanky Leg'},
+  t: {name: 'tootboot',
+      caption: 'T - Toot It and Boot It'},
+  u: {name: 'urkel',
+      caption: 'U - The Urkel'},
+  v: {name: 'vonmiller',
+      caption: 'V - Von Miller'},
+  w: {name: 'walkitout',
+      caption: 'W - Walk It Out'},
+  x: {name: 'x-crisscross',
+      caption: 'X - Criss-Cross'},
+  y: {name: 'ymca',
+      caption: 'Y - YMCA'},
+  z: {name: 'zigzag',
+      caption: 'Z - The Zig-Zag'},
+  };
+
--- Personal-Project/mySite/src/js/main.js
+++ Personal-Project/mySite/src/js/main.js
@@ -3,27 +3,27 @@
 $(document).on('ready', function() {
   console.log('sanity check!');
   var userInput = $('#userInput');
-  $('form').on('submit', function(el) {
-    el.preventDefault();
-    console.log(userInput.val());
+  $('form').on('submit', function(event) {
+    event.preventDefault();
     submitLetters(userInput.val());
   });
 });
 
-function submitLetters(input){
-    for (var i=0; i<input.length; i++) {
-      if (input[i] === 'a') {
-        console.log(input);
-        $('#slide').append('<img src = "../whitegifs/alligator.gif">');
-      }
-      else {
-        console.log(input);
-      }
+function submitLetters(userInput){
+    var inputToLettersOnly = userInput.replace(/\s+/g, '-').replace('/[^a-zA-Z-]/g', '').toLowerCase();
+    $('#slides').empty();
+    var keys=Object.keys(MyGifs);
+    for (var i=0; i<inputToLettersOnly.length; i++) {
+      for (var n=0; n<keys.length; n++) {
+        // console.log('MyGifs[keys[n]]=',MyGifs[keys[n]].name);
+        if (inputToLettersOnly[i] === keys[n]) {
+          $('#slides').append(gifStr(MyGifs[keys[n]].name, MyGifs[keys[n]].caption));
+        }
+        else {
+          // console.log('gifstr', gifStr(MyGifs[keys[n]].name));
+          console.log("Error in submitLetters fn AHHHHHHHHHHHH!!!!!");
+        }
     }
+    $("#slides .item").first().addClass('active');
   }
-
-// $('form').on('submit', function(event){
-//     event.preventDefault();
-//     console.log(userInput);
-//     submitLetters(userInput);
-//   });
+}
